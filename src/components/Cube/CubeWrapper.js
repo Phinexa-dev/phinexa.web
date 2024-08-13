c0d3287618b780d04d1f4785d365f8832d0b4c67
Dinuka Ashan Amarasinghe <dinukaamarasinghe817@gmail.com>
2024-08-13 13:41:00 +0530

    Add turn and rotate instruction

--- src/components/Cube/CubeWrapper.js
+++ src/components/Cube/CubeWrapper.js
@@ -91,7 +91,25 @@ function CubeWrapper() {
                           </g>
                         </svg>
                       </div>
-                      <div className='instruction-btn' onClick={()=> setInstructionStep(0)}>Got It</div>
+                    <div className='instruction-btn' onClick={()=> setInstructionStep(3)}>Next</div>
+                    </div>
+                </div>
+            )}
+            {instructionStep === 3 && (
+                <div className="instructions">
+                    <div className="inner-border">
+                      <p>Touch outside and drag to turn or rotate the cube.</p>
+                      <div className="svgs">
+                        <svg className='gesture custom-swiper' id="Swipe-horizontal_1" data-name="Swipe horizontal 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+                          <path class="hand-x" d="M139.93,113.56l-41.12-6.93V76.08a9.25,9.25,0,0,0-9.25-9.25h0a9.25,9.25,0,0,0-9.25,9.25v57.36L71,122.65c-3.61-3.61-8.44-3.89-13.08,0,0,0-7.24,5.84-3.83,9.25l34,34h42.63a9.25,9.25,0,0,0,9.07-7.43l6.82-34.09A9.28,9.28,0,0,0,139.93,113.56Z"/>
+                          <svg width="200" height="67" viewBox="0 0 200 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M64 38.0273L94.5338 55.3952" stroke="white" stroke-width="5" stroke-linecap="round"/>
+                          <path d="M94.5342 55.3574L135.096 31.7626" stroke="white" stroke-width="5" stroke-linecap="round"/>
+                          <path d="M94.6025 55.4961L93.5723 14.3009" stroke="white" stroke-width="5" stroke-linecap="round"/>
+                          </svg>
+                        </svg>
+                      </div>
+                      <div className='instruction-btn' onClick={()=> setInstructionStep(4)}>Got It</div>
                     </div>
                 </div>
             )}
